3c492e4c6d7ad6bae9a42cbb902cdef700d22416
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-03 15:40:09 -0300

    feat: add styles folder with GlobalStyles and theme configuration

---
+++ src/styles/theme.jsx
@@ -0,0 +1,10 @@
+const theme = {
+    colors: {
+      primary: '#ffbe3b',
+      secondary: '#222222',
+      background: '#efefef',
+      text: '#212529',
+    },
+  };
+  
+  export default theme;
