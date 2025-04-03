8868138b963896fa6c478fbbea98ef8e5eda7778
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-03 15:41:54 -0300

    Hello, World!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-function App() {
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import theme from "./styles/Theme";
 
+function App() {
   return (
     <>
-
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <p>Olá, mundo!</p>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
