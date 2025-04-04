dfc082231f5b54e9b8cfdbc1b4c94fa95e3a3e92
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-04 18:37:40 -0300

    chore: updates in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import theme from "./styles/Theme";
+import AppRoutes from "./routes/AppRoutes";
+import Header from "./layout/Header/Header";
+import Footer from "./layout/Footer/Footer";
 
 function App() {
   return (
-    <>
+    <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <p>Olá, mundo!</p>
+        <Header />
+        <AppRoutes />
+        <Footer />
       </ThemeProvider>
-    </>
+    </Router>
   );
 }
 
