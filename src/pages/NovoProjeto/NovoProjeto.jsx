55f23d4acc0b255da6b17396317e1d8e08c73c2e
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-05 15:50:11 -0300

    feat: add NovoProjeto page

---
+++ src/pages/NovoProjeto/NovoProjeto.jsx
@@ -0,0 +1,11 @@
+import Main from "../../components/Main/Main";
+
+function NovoProjeto() {
+  return (
+    <Main width="1200px" justify="space-between" direction="column">
+      <h1>ProjetoNovo</h1>
+    </Main>
+  );
+}
+
+export default NovoProjeto;
