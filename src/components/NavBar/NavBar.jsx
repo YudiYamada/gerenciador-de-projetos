456e76eeae4c657c7704a759fdf9a6771f684b55
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-05 14:42:11 -0300

    feat: add responsive hamburger menu to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { NavbarContainer, NavList, NavLink } from "./NavBarStyles";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { NavbarContainer, NavList, NavLink, HamburgerButton } from "./NavBarStyles";
+import { useToggleMenuHamburger } from "../../hooks/useToggleMenuHamburger";
 
-const NavBar = () => (
-  <NavbarContainer>
-    <NavList>
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/projetos">Projetos</NavLink></li>
-      <li><NavLink to="/empresa">Empresa</NavLink></li>
-      <li><NavLink to="/contato">Contato</NavLink></li>
-    </NavList>
-  </NavbarContainer>
-);
+const NavBar = () => {
+  const { isOpen, toggleMenuHamburger } = useToggleMenuHamburger();
+
+  return (
+    <NavbarContainer>
+      <HamburgerButton isOpen={isOpen} onClick={toggleMenuHamburger}>
+        <RxHamburgerMenu />
+      </HamburgerButton>
+      <NavList isOpen={isOpen}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/projetos">Projetos</NavLink>
+        </li>
+        <li>
+          <NavLink to="/empresa">Empresa</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contato">Contato</NavLink>
+        </li>
+      </NavList>
+    </NavbarContainer>
+  );
+};
 
 export default NavBar;
--- src/components/NavBar/NavBarStyles.jsx
+++ src/components/NavBar/NavBarStyles.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export const NavbarContainer = styled.nav`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 1em;
 `;
 
@@ -12,6 +13,17 @@ export const NavList = styled.ul`
   list-style: none;
   align-items: center;
   gap: 20px;
+
+  @media (max-width: 500px) {
+    flex-direction: column;
+    position: absolute;
+    top: 90px;
+    left: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+    background: ${({ theme }) => theme.colors.secondary};
+    width: 100%;
+    padding: 1em;
+    transition: 0.3s ease-in-out;
+  }
 `;
 
 export const NavLink = styled(Link)`
@@ -22,5 +34,21 @@ export const NavLink = styled(Link)`
 
   &:hover {
     color: ${({ theme }) => theme.colors.primary};
+    cursor: pointer;
+  }
+`;
+
+export const HamburgerButton = styled.button`
+  display: none;
+  background: none;
+  border: none;
+  font-size: 2em;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+  color: ${({ theme }) => theme.colors.primary};
+
+  @media (max-width: 500px) {
+    display: block;
+    transform: ${({ isOpen }) => (isOpen ? "rotate(90deg)" : "rotate(0deg)")};
   }
 `;
