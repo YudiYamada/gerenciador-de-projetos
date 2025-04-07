de63ecc48580a61594afa63762d511c424436dde
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-07 18:54:45 -0300

    feat(form): add dynamic options to Select component

--- src/components/Form/Selects/Select.jsx
+++ src/components/Form/Selects/Select.jsx
@@ -1,11 +1,21 @@
 import { FormControl } from "./SelectStyles";
- 
+
 function Select({ text, name, options, handleOnChange, value }) {
   return (
     <FormControl>
       <label htmlFor={name}>{text}</label>
-      <select name={name} id={name}>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ""}
+      >
         <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </FormControl>
   );
