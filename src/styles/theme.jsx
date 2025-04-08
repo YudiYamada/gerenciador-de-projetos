b11c6c1c3bff5bb5ac35915ec1a7f427090f4509
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-08 15:55:42 -0300

    feat: add successColors and errorColors to theme

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -5,6 +5,17 @@ const theme = {
     tertiary: "#7b7b7b",
     background: "#efefef",
     text: "#fff",
+    border: "#000",
+  },
+  successColors: {
+    color: "#155724",
+    background: "#d4edda",
+    border: "#c3e6cb",
+  },
+  errorColors: {
+    color: "#721c24",
+    background: "#f8d7da",
+    border: "#f5c6cb",
   },
 };
 
