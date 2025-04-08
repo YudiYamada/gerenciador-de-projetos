1075498ce560e2e734e4a59c91b901546dafb5d3
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-08 18:31:31 -0300

    refactor: update Projetos page structure and functionality

--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -1,10 +1,19 @@
+import { useLocation } from "react-router-dom";
 import Main from "../../components/Main/Main";
+import Message from "../../components/Message/Message";
+import {} from "./ProjetosStyle";
 
 function Projetos() {
+  const location = useLocation();
+  const message = location.state?.message || "";
+
   return (
     <Main justify="space-around" direction="column">
-      <h1>Projetos</h1>
-      <p>Confira alguns dos nossos principais projetos.</p>
+      <div>
+        <h1>Meus Projetos</h1>
+        <a href="#">Novo Projeto</a>
+      </div>
+      {message && <Message msg={message} type="success" />}
     </Main>
   );
 }
