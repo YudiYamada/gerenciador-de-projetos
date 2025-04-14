89b089214f00740070de88e12c580dbd2af99deb
Yudi Yamada <yudiyamada10@gmail.com>
2025-04-14 18:22:30 -0300

    feat: add Loading component to Projetos.jsx

--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -1,19 +1,42 @@
 import { useLocation } from "react-router-dom";
 import Message from "../../components/Message/Message";
-import {} from "./ProjetosStyle";
+import { ProjectContainer, TitleContainer } from "./ProjetosStyle";
+import Container from "../../components/Container/Container";
+import LinkButton from "../../components/LinkButton/LinkButton";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import useProjects from "../../hooks/useProjects";
+import Loading from "../../components/Loading/Loading";
 
 function Projetos() {
   const location = useLocation();
   const message = location.state?.message || "";
+  const { projects, loading, error } = useProjects();
 
   return (
-    <>
-      <div>
+    <ProjectContainer>
+      <TitleContainer>
         <h1>Meus Projetos</h1>
-        <a href="#">Novo Projeto</a>
-      </div>
+        <LinkButton to="/novoprojeto" text="Criar Projeto" />
+      </TitleContainer>
       {message && <Message msg={message} type="success" />}
-    </>
+      {error && <p>Erro ao carregar projetos: {error}</p>}
+      <Container justify="start">
+        {loading ? (
+          <Loading />
+        ) : projects.length > 0 ? (
+          projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category?.name || "Categoria desconhecida"}
+            />
+          ))
+        ) : (
+          !loading && !error && <p>Não há projetos cadastrados!</p>
+        )}
+      </Container>
+    </ProjectContainer>
   );
 }
 
---
+++ src/pages/Projetos/ProjetosStyle.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ProjectContainer = styled.div`
+  padding: 2em;
+  width: 100%;
+  height: 75vh;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 2em;
+`;
