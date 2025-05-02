fd6f15f96f9127e4e35b75ed4a52e3d4e62aa2df
Yudi Yamada <yudiyamada10@gmail.com>
2025-05-02 16:42:46 -0300

    deploy configuration

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,21 +4,23 @@ import Home from "../pages/Home/Home";
 import Projetos from "../pages/Projetos/Projetos";
 import Empresa from "../pages/Empresa/Empresa";
 import Contato from "../pages/Contato/Contato";
-import NovoProjeto from "../pages/NovoProjeto/NovoProjeto"
+import NovoProjeto from "../pages/NovoProjeto/NovoProjeto";
 import Projeto from "../pages/Projeto/Projeto";
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/empresa" element={<Empresa />} />
-        <Route path="/projetos" element={<Projetos />} />
-        <Route path="/novoprojeto" element={<NovoProjeto />} />
-        <Route path="/projeto/:id" element={<Projeto />} />
-      </Route>
-    </Routes>
+    <Router basename="/gerenciador-de-projetos">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/empresa" element={<Empresa />} />
+          <Route path="/projetos" element={<Projetos />} />
+          <Route path="/novoprojeto" element={<NovoProjeto />} />
+          <Route path="/projeto/:id" element={<Projeto />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
